80fa43a2e748c7a4c2a7e560e29383fd8a0f96c4
Nick Gerrets <ngerrets@f1r2s17.codam.nl>
2022-06-17 14:09:13 +0200

    Refraction calculation added

--- src/vec3/vec3_advanced.c
+++ src/vec3/vec3_advanced.c
@@ -6,7 +6,7 @@
 /*   By: mjoosten <mjoosten@student.42.fr>            +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2022/06/02 14:06:03 by ngerrets      #+#    #+#                 */
-/*   Updated: 2022/06/17 12:39:24 by ngerrets      ########   odam.nl         */
+/*   Updated: 2022/06/17 14:08:48 by ngerrets      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,19 +34,70 @@ inline void Refract(
     out = eta * incidentVec - (eta * N_dot_I + sqrtf(k)) * normal;
 }
 */
+/*
+Vec3 computeRefraction(const Vec3& I, const Vec3& N, const float &ior, Vec3& intersection) {
+    Vec3 normal = N; normal.normalize();
+    normal = normal;
+    Vec3 incident = I; incident.normalize();
+    float cosi = incident.dot(normal);
+    float n1, n2;
+    if (cosi > 0.0f) { 
+        //Incident and normal have same direction, INSIDE sphere
+        n1 = ior;
+        n2 = 1.0f;
+        normal = -normal;
+    } else { 
+        //Incident and normal have opposite direction, OUTSIDE sphere
+        n1 = 1.0f;
+        n2 = ior;
+        cosi = -cosi;
+    }
+
+    float eta = n1 / n2;
+    float k = 1.0f - (eta * eta) * (1.0f - cosi * cosi);
+
+    if (k < 0.0f) {   
+        //internal reflection
+        Vec3 reflectionRay = computeReflection(incident, normal);
+        intersection = intersection + (normal * BIAS);
+        return reflectionRay;
+    } else {
+        Vec3 refractionVector = incident * eta + normal * (eta * cosi - sqrt(k));
+        refractionVector.normalize();
+        intersection = intersection - (normal * BIAS);
+        return refractionVector;
+    }
+}
+*/
 
 t_v3	vec3_calc_refraction(t_v3 incoming, t_v3 normal, double strength)
 {
-	double	dot;
+	double	cosi;
+	double	n1;
+	double	n2;
+	double	eta;
 	double	k;
 	t_v3	refr;
 
-	dot = vec3_dot(normal, incoming);
-	k = 1.0 - strength * strength * (1.0 - dot * dot);
+	cosi = vec3_dot(incoming, normal);
+	if (cosi > 0.0f)
+	{
+		n1 = strength;
+		n2 = 1.0;
+		normal = vec3_inv(normal);
+	}
+	else
+	{
+		n1 = 1.0;
+		n2 = strength;
+		cosi = -cosi;
+	}
+	eta = n1 / n2;
+	k = 1.0 - (eta * eta) * (1.0 - cosi * cosi);
 	if (k < 0.0)
-		return (incoming);
-	refr = vec3_sub(vec3_mul(incoming, strength), vec3_mul(normal, strength * dot * sqrt(k)));
-	refr = vec3_norm(refr);
+		refr = vec3_calc_reflection(incoming, normal);
+	else
+		refr = vec3_add( vec3_mul(incoming, eta), vec3_mul(normal, eta * cosi * sqrt(k)) );
 	return (refr);
 }
 

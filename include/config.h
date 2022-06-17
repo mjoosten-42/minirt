0556dfd94c4410e1514115a407151c36060c7fa8
Maarten Joosten <mjoosten@student.codam.nl>
2022-06-17 17:12:37 +0200

    more merge

--- include/config.h
+++ include/config.h
@@ -1,22 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-<<<<<<< HEAD
 /*                                                        :::      ::::::::   */
 /*   config.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 11:53:24 by mjoosten          #+#    #+#             */
-/*   Updated: 2022/06/17 17:09:43 by mjoosten         ###   ########.fr       */
-=======
-/*                                                        ::::::::            */
-/*   config.h                                           :+:    :+:            */
-/*                                                     +:+                    */
-/*   By: mjoosten <mjoosten@student.42.fr>            +#+                     */
-/*                                                   +#+                      */
-/*   Created: 2022/06/10 11:53:24 by mjoosten      #+#    #+#                 */
-/*   Updated: 2022/06/17 16:18:06 by ngerrets      ########   odam.nl         */
->>>>>>> 423b25f2f0b485ad344f53ff591747d2d35a8cd2
+/*   Updated: 2022/06/17 17:12:12 by mjoosten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +23,7 @@
 # define WINDOW_RESIZE 0
 
 /* threads.c */
-<<<<<<< HEAD
 # define NB_THREADS 7
-=======
-# define NB_THREADS 8
->>>>>>> 423b25f2f0b485ad344f53ff591747d2d35a8cd2
 
 /* raycasting.c */
 # define RAY_MAX_BOUNCES 8
@@ -46,10 +32,5 @@
 # define AA 1
 
 /* shape.c */
-<<<<<<< HEAD
-# define DEFAULT_PHONG 10
-
-=======
 # define DEFAULT_PHONG 4
->>>>>>> 423b25f2f0b485ad344f53ff591747d2d35a8cd2
 #endif

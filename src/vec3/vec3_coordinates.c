bd2c382ff2a018ffaea64f0974ac1ac5cd591d97
Maarten Joosten <mjoosten@student.codam.nl>
2022-07-07 11:44:20 +0200

    oneline latitude

--- src/vec3/vec3_coordinates.c
+++ src/vec3/vec3_coordinates.c
@@ -6,7 +6,7 @@
 /*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 14:52:24 by ngerrets          #+#    #+#             */
-/*   Updated: 2022/07/07 11:33:22 by mjoosten         ###   ########.fr       */
+/*   Updated: 2022/07/07 11:44:07 by mjoosten         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,7 @@
 
 double	vec3_get_latitude(t_v3 v)
 {
-	double	lat;
-
-	lat = atan2(v.y, (sqrt(v.x * v.x + v.z * v.z))) / M_PI_2;
-	return (lat);
+	return (atan2(v.y, (sqrt(v.x * v.x + v.z * v.z))) / M_PI_2);
 }
 
 double	vec3_get_longitude(t_v3 v)

629c7cf1e11998271622574c69b2b3b1c35c4546
Nick Gerrets <ngerrets@f1r4s6.codam.nl>
2022-07-07 16:20:46 +0200

    Added more to planets.rt scene file

--- include/config.h
+++ include/config.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   config.h                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mjoosten <mjoosten@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/10 11:53:24 by mjoosten          #+#    #+#             */
-/*   Updated: 2022/07/07 15:25:19 by mjoosten         ###   ########.fr       */
+/*                                                        ::::::::            */
+/*   config.h                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: mjoosten <mjoosten@student.42.fr>            +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2022/06/10 11:53:24 by mjoosten      #+#    #+#                 */
+/*   Updated: 2022/07/07 16:16:06 by ngerrets      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@
 # endif
 
 /* program.init.c */
-# define WINDOW_W 1024
-# define WINDOW_H 720
+# define WINDOW_W 1600
+# define WINDOW_H 1080
 # define WINDOW_TITLE "minirt"
 # define WINDOW_RESIZE 0
 
 /* threads.c */
-# define NB_THREADS 7
+# define NB_THREADS 8
 
 /* raycasting.c */
 # define RAY_MAX_BOUNCES 8
